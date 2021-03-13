0abb904f5f2bdfcb03e46bff9be8a921b8c58358
waleedzaman786 <waleedzaman41@gmail.com>
2021-03-14 00:50:15 +0500

    Add files via upload

---
+++ module/Application/Module.php
@@ -0,0 +1,39 @@
+<?php
+namespace Application;
+
+use Zend\Console\Console;
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Zend\View\Helper\Navigation;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+        $sm = $e->getApplication()->getServiceManager();
+
+        // ACL for Navigation
+        $this->setupNavigation($sm);
+    }
+
+    protected function setupNavigation(ServiceLocatorInterface $sm)
+    {
+        // Add ACL information to the Navigation view helper
+        if (!Console::isConsole()) {
+            $authorize = $sm->get('BjyAuthorize\Service\Authorize');
+            $acl = $authorize->getAcl();
+            $role = $authorize->getIdentity();
+            Navigation::setDefaultAcl($acl);
+            Navigation::setDefaultRole($role);
+        }
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+}
